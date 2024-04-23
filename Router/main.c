2fabb550e9ca1f0c815bf6398e0f0bff33ae33a3
Praveen Singh <ingpraveen@hotmail.com>
2024-04-23 10:30:30 +0530

    Starting out with some definitions hand headers.

--- Router/main.c
+++ Router/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <c-algorithm.h>
+#include <router_defs.h>
 
-char* ctor(char* s)
+static char* ctor(char* s)
 {
 	char* p;
 	if ((p = (char*)malloc(strlen(s) + 1)) != NULL)
@@ -11,18 +11,32 @@ char* ctor(char* s)
 	return p;
 }
 
-void dtor(char* s)
+static void dtor(char* s)
 {
 	free(s);
 	return;
 }
 
+static void itr(char* s)
+{
+	printf("%s ", s);
+	return;
+}
 
 int main(int argc, char* argv[])
 {
 	printf("Router: Hello World\n");
-	struct WSet* a;
+	struct WSet* a, *b, *c;
 	a = WCreateSet((WCMPFP)strcmp, (WCTRFP)ctor, (WDTRFP)dtor);
+	b = WCreateSet((WCMPFP)strcmp, (WCTRFP)ctor, (WDTRFP)dtor);
+	WInsertKeySet(a, "Where");
+	WInsertKeySet(a, "is");
+	WInsertKeySet(a, "Tom");
+	WUnionOfSet(a, b, &c);
+	WIteratorSet(c, (void (*)(void*))itr);
+	putchar('\n');
 	WDeleteSet(a);
+	WDeleteSet(b);
+	WDeleteSet(c);
 	return 0;
 }
---
+++ Router/router_defs.h
@@ -0,0 +1,9 @@
+#ifndef ROUTER_DEFS_H
+#define ROUTER_DEFS_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <c-algorithm.h>
+
+
+#endif // ROUTER_DEFS_H
